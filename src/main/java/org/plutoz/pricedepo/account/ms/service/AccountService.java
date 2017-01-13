147b5d3b9d7d5428a393d0de6f1c99ec76f1798e
László Csatlós <plutoz01@gmail.com>
2017-01-13 22:34:43 +0100

    Use common-lib for Services

--- src/main/java/org/plutoz/pricedepo/account/ms/service/AccountService.java
+++ src/main/java/org/plutoz/pricedepo/account/ms/service/AccountService.java
@@ -1,7 +1,7 @@
 package org.plutoz.pricedepo.account.ms.service;
 
 import org.plutoz.pricedepo.account.ms.domain.Account;
+import org.plutoz.pricedepo.common.service.CrudService;
 
-public interface AccountService {
-	public Iterable<Account> findAll();
+public interface AccountService extends CrudService<Account, Long> {
 }
--- src/main/java/org/plutoz/pricedepo/account/ms/service/AccountServiceImpl.java
+++ src/main/java/org/plutoz/pricedepo/account/ms/service/AccountServiceImpl.java
@@ -1,23 +1,19 @@
 package org.plutoz.pricedepo.account.ms.service;
 
+import javax.transaction.Transactional;
+
 import org.plutoz.pricedepo.account.ms.domain.Account;
 import org.plutoz.pricedepo.account.ms.repository.AccountRepository;
+import org.plutoz.pricedepo.common.service.AbstractCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccountServiceImpl implements AccountService {
-
-	private AccountRepository accountRepository;
+@Transactional
+public class AccountServiceImpl extends AbstractCrudService<Account, Long> implements AccountService {
 	
 	@Autowired
 	public AccountServiceImpl(AccountRepository accountRepository){
-		this.accountRepository = accountRepository;
+		super(accountRepository);
 	}
-	
-	@Override
-	public Iterable<Account> findAll() {
-		return accountRepository.findAll();
-	}
-
 }
